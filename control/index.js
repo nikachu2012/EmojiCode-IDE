bdf0fe6c0e330d668f3e855c1f9c3e6b6bdc230f
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-07-30 02:32:51 +0900

    [add] 実行環境の完成

--- control/index.js
+++ control/index.js
@@ -1,19 +1,42 @@
 const pushFlag = () => {
     const code = Blockly.JavaScript.workspaceToCode();
-    console.log(code)
-
-    try {
-        const AST = acorn.parse('banbah();', { ecmaVersion: 8 });
-        console.log(AST)
-    } catch (error) {
-        alert(error)
-    }
 
     const runBoolean = window.confirm('実行してもよろしいですか？')
 
     if (runBoolean) {
-        
+
+        document.getElementById('console').innerHTML = `<div class="logArea" id="logArea"></div>`
+        document.getElementById('outputJS').innerHTML = `<pre><code>${code}</code></pre>`
+
+
+        try {
+            const AST = acorn.parse(code, { ecmaVersion: 8 });
+            document.getElementById('error').innerHTML =
+                `<div class="success material-symbols-outlined" style="font-size: 40pt;">done</div>
+            <span class="text">エラーは発見されませんでした。</span>`;
+            document.getElementById('outputAST').innerHTML = `<pre style="word-break:none;"><code>${JSON.stringify(AST)}</code></pre>`
+            hljs.initHighlighting();
+
+            runCode(code);
+
+            toastr.success('コードの実行が完了しました。');
+        } catch (error) {
+            document.getElementById('error').innerHTML =
+            `<div class="error material-symbols-outlined" style="font-size: 40pt;">close</div>
+            <span class="text">エラーが発見されました。</span>
+            <div class="message">
+              ${error}
+            </div>`;
+            document.getElementById('outputAST').innerHTML = `エラーが発見されたため、ASTを出力できませんでした。`
+
+            toastr.error('エラーが発見されました。<br>修正してください。');
+        }
+
+
+
+
+
     } else {
-        
+
     }
 };
