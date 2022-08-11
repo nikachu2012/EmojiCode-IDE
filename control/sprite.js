2f4c48bf3875e6e7b15d600e6eb4fb4035fcb282
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-12 03:06:22 +0900

    [add] スプライトタブの追加

---
+++ control/sprite.js
@@ -0,0 +1,11 @@
+const option = {
+    firstBackground: '#FFFFFF'
+}
+emojisp.create('run-canvas', option)
+
+emojisp.spriteListWrite = () =>{
+    const key = Object.keys(spriteOption)
+    if(key.length == 0){
+        document.getElementById('list').innerHTML('<div class="no-run">スプライトが作成されていません。</div>')
+    }
+}
