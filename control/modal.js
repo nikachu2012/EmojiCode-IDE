d6590d619e3c20d804e7a90bee046d1f5ec8119f
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-13 00:24:03 +0900

    [add] アコーディオンメニューで画像のサイズが変更できるように修正

--- control/modal.js
+++ control/modal.js
@@ -73,12 +73,61 @@ const filebase64 = () => {
 
 const fileNameUpdate = () => {
     const upload = document.getElementById('fileUpload').files[0]
+    const pictureSetting = document.getElementById('pictureSetting')
     
-    if(upload !== undefined){
+    
+    const uploadImage = document.querySelector('#fileUpload')
+    const file = uploadImage.files[0]
+    if (upload !== undefined) {
         document.getElementById('uploadArea').className += ' selected'
+
+        const reader = new FileReader()
+
+        reader.onload = (event) => {
+            const base64Text = reader.result;
+            const img = new Image();
+            img.src = base64Text;
+
+            img.onload = () => {
+                pictureSetting.innerHTML = `
+                <input type="checkbox" name="size" id="size" onchange="connecttext('pictureWidth', 'pictureHeight', this.checked)">
+                <label for="size">サイズを変更する</label>
+                <input type="number" id="pictureWidth" placeholder="横幅" value="${img.naturalWidth}" disabled="disabled">px<br>
+                <input type="number" id="pictureHeight" placeholder="縦幅" value="${img.naturalHeight}" disabled="disabled">px<br>
+                <div>0を入力すると自動計算します。</div>
+                `
+            }
+           
+        }
+        if (file) {
+            reader.readAsDataURL(file)
+        }
     }
     else {
         document.getElementById('uploadArea').className = document.getElementById('uploadArea').className.replace(' selected', '')
+        pictureSetting.innerHTML = `画像を選択すると表示されます。`
     }
-    
+
+}
+
+const dt_tags = document.querySelectorAll('.accordion dt');
+dt_tags.forEach(dt => {
+    dt.addEventListener('click', () => {
+        dt.classList.toggle('open');
+        dt.nextElementSibling.classList.toggle('open');
+    })
+});
+
+
+function connecttext(textid, textid2, ischecked) {
+    if (ischecked == true) {
+        // チェックが入っていたら有効化
+        document.getElementById(textid).disabled = false;
+        document.getElementById(textid2).disabled = false;
+    }
+    else {
+        // チェックが入っていなかったら無効化
+        document.getElementById(textid).disabled = true;
+        document.getElementById(textid2).disabled = true;
+    }
 }
