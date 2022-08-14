2489de97615ca8c638784a03d8c0d20792e7f958
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-15 00:38:07 +0900

    [add] 削除機能の追加

--- control/sprite.js
+++ control/sprite.js
@@ -9,15 +9,15 @@ spriteListWrite = () => {
         document.getElementById('spriteList').innerHTML = '<div class="no-run">スプライトが作成されていません。</div>';
     }
     else {
+        const spriteList = document.getElementById('spriteList');
+        spriteList.innerHTML = '';
         key.forEach(element => {
-            const spriteList = document.getElementById('spriteList');
-            spriteList.innerHTML = '';
 
             let spriteElement = document.createElement('div');
-            spriteElement.innerHTML = `<div class="sprite-list-icon">
+            spriteElement.innerHTML = `<div class="sprite-list-icon" id="spriteList_${element}">
               <div class="image-base"><img src="${spriteOption[element].url}" class="icon"></div>
               <div class="id" title="${spriteOption[element].id}"><span class="text">${spriteOption[element].id}</span></div>
-              <button class="material-symbols-outlined close-button">close</button>
+              <button class="material-symbols-outlined close-button" onclick="spriteDelList(\`${element}\`)">close</button>
             </div>`
 
             spriteList.appendChild(spriteElement)
@@ -25,3 +25,35 @@ spriteListWrite = () => {
     }
 }
 
+
+spriteDelList = (id) => {
+    try {
+
+
+        const result = confirm('本当に削除しますか？')
+
+        if (result) {
+            if (Object.keys(spriteOption).includes(id)) {
+                emojisp.deleteSprite(id)
+                document.getElementById(`spriteList_${id}`).remove();
+
+                if (Object.keys(spriteOption).length == 0) {
+                    document.getElementById('spriteList').innerHTML = '<div class="no-run">スプライトが作成されていません。</div>';
+                }
+            }
+            else {
+                alert('指定されたスプライトが存在していません。')
+                console.error('EmojiCode IDE: 指定されたスプライトが存在していません。')
+                return null
+            }
+        }
+        else{}
+
+
+    } catch (error) {
+        alert('EmojiCode IDE Error detect!\nPlease see DevTools.')
+        console.log(error)
+    }
+
+
+}
