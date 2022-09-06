3b1ebca323d4edb0c27a551e7a13d259b25364fb
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-09-07 00:54:04 +0900

    [add] ファイル保存のロジックを追加

--- workspace.js
+++ workspace.js
@@ -8,6 +8,10 @@ import Ja from 'blockly/msg/ja'
 import './emojicode/emojicode_def'
 import './emojicode/emojicode_stub'
 
+import * as localforage from 'localforage';
+import FileSaver from 'file-saver';
+
+
 Promise.all(
     ["./emojicode/build/workspace.xml", "./emojicode/build/toolbox.xml"].map(async file => {
         return fetch(file).then(
@@ -71,9 +75,181 @@ Promise.all(
         attributes: true,
         attributeFilter: ['style']
     })
+
+
+    document.querySelector('#quickSave').addEventListener('click', () => {
+
+        const id = window.prompt('保存名を入力してください')
+
+        if (id === '') {
+
+        }
+        else {
+            const xmlDom = Blockly.Xml.workspaceToDom(workspace);
+            const xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+
+            let savefile = {}
+
+            savefile.blocklyXML = encodeURI(xmlText)
+            savefile.soundDetail = encodeURI(JSON.stringify(emojiau.soundDetail));
+            savefile.spriteOption = encodeURI(JSON.stringify(spriteOption));
+            savefile.saveDate = Date.now().toString();
+
+
+            console.log(savefile)
+            localforage.setItem(id, JSON.stringify(savefile)).then();
+        }
+    })
+
+    const loadFile = (id) => {
+        closeModal();
+
+        localforage.getItem(id).then(function (value) {
+            const data = JSON.parse(value)
+
+            const convertSpriteOption = JSON.parse(decodeURI(data.spriteOption));
+            const convertSoundDetail = JSON.parse(decodeURI(data.soundDetail));
+
+            console.log(convertSpriteOption)
+            var xml = Blockly.Xml.textToDom(decodeURI(data.blocklyXML));
+            workspace.clear();
+            Blockly.Xml.domToWorkspace(xml, workspace);
+
+            emojiau.soundDetail = {}
+            spriteOption = {}
+
+
+            Object.keys(convertSpriteOption).forEach((e) => {
+                emojisp.createSprite(convertSpriteOption[e])
+            })
+            emojiau.detailAdd(convertSoundDetail)
+
+
+            spriteListWrite();
+            soundListWrite();
+        }).catch(function (err) {
+            alert('EmojiCode IDE Error detect!\nPlease see DevTools.')
+            console.log(err);
+        });
+
+    }
+
+    document.querySelector('#fileSaveButton').addEventListener('click', () => {
+        const id = window.prompt('ファイル名を入力してください。')
+
+        if (id === '') {
+
+        }
+        else {
+            const xmlDom = Blockly.Xml.workspaceToDom(workspace);
+            const xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+
+            let savefile = {}
+
+            savefile.blocklyXML = encodeURI(xmlText)
+            savefile.soundDetail = encodeURI(JSON.stringify(emojiau.soundDetail));
+            savefile.spriteOption = encodeURI(JSON.stringify(spriteOption));
+            savefile.saveDate = Date.now().toString();
+
+
+            console.log(savefile)
+
+            var blob = new Blob([JSON.stringify(savefile)], { type: "text/plain;charset=utf-8" });
+            FileSaver.saveAs(blob, `${id}.eci`);
+        }
+    })
+
+
+    document.querySelector('#allDeleteIDB').addEventListener('click', () => {
+        localforage.clear().then(function () {
+            // Run this code once the database has been entirely deleted.
+            console.log('Database is now empty.');
+        }).catch(function (err) {
+            alert('EmojiCode IDE Error detect!\nPlease see DevTools.')
+            console.log(err);
+        });
+    })
+
+    document.querySelector('#quickLoad').addEventListener('click', () => {
+        localforage.keys().then(function (keys) {
+            // An array of all the key names.
+            const selectModal = document.querySelector("#listBody")
+            selectModal.innerHTML = null
+            keys.forEach((e, i) => {
+                if (e == '' || e == null) {
+
+                }
+                else {
+                    let fileList = document.createElement('button')
+                    fileList.className = "fileList";
+                    fileList.id = `ide_${e}_fileList`
+                    fileList.innerHTML = `${e}`
+                    selectModal.appendChild(fileList)
+                    document.querySelector(`#ide_${e}_fileList`).addEventListener('click', () => {
+                        loadFile(`${e}`)
+                    })
+                }
+            })
+
+
+            console.log(keys);
+        }).catch(function (err) {
+            alert('EmojiCode IDE Error detect!\nPlease see DevTools.')
+            console.log(err);
+        });
+
+        displayModal('modal5')
+    })
+
+    document.querySelector('#saveFileConfirm').addEventListener('click', () => {
+        try {
+            const fileinput = document.querySelector('#saveFileLoad');
+            const file = fileinput.files[0]
+
+            const reader = new FileReader();
+
+            reader.onload = (event) => {
+                const text = event.target.result;
+                console.log(text);
+
+                const data = JSON.parse(text)
+
+                const convertSpriteOption = JSON.parse(decodeURI(data.spriteOption));
+                const convertSoundDetail = JSON.parse(decodeURI(data.soundDetail));
+
+                console.log(convertSpriteOption)
+                var xml = Blockly.Xml.textToDom(decodeURI(data.blocklyXML));
+                workspace.clear();
+                Blockly.Xml.domToWorkspace(xml, workspace);
+
+                emojiau.soundDetail = {}
+                spriteOption = {}
+
+
+                Object.keys(convertSpriteOption).forEach((e) => {
+                    emojisp.createSprite(convertSpriteOption[e])
+                })
+                emojiau.detailAdd(convertSoundDetail)
+
+
+                spriteListWrite();
+                soundListWrite();
+            }
+
+            reader.readAsText(file)
+            closeModal();
+            fileinput.value = '';
+            document.getElementById('saveFileArea').className = document.getElementById('saveFileArea').className.replace(' selected', '')
+        } catch (error) {
+            alert('EmojiCode IDE Error detect!\nPlease see DevTools.')
+            console.log(error);
+        }
+
+    })
+
+
 });
 
 Blockly.HSV_SATURATION = 0.45;
 Blockly.HSV_VALUE = 0.65;
-
 
