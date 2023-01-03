6d4a83f7ebeed28910fca42ea241ffcaa8a69ecb
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-01-04 03:05:04 +0900

    [fix] 1桁の数字の月になると00になる問題を解決

--- control/titleButton.js
+++ control/titleButton.js
@@ -253,7 +253,7 @@ const updateQuickSaveControl = () => {
 const clockDate = (UNIXdate) => {
     const date = new Date(UNIXdate)
 
-    return `${date.getFullYear()}/${date.getMonth().toString().padStart(2, "0")}/${date.getDay().toString().padStart(2, "0")} ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`
+    return `${date.getFullYear()}/${('0' + (date.getMonth() + 1)).slice(-2)}/${('0' + date.getDate()).slice(-2)} ${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${('0' + date.getSeconds()).slice(-2)}`
 }
 
 
